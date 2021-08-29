9b93884f5562b7b066b9b09a041babf0b2c51a68
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-08-29 08:31:41 +0530

    Update Astrorun.py

--- Astrorun.py
+++ Astrorun.py
@@ -1,8 +1,8 @@
 from os import system
 
 system(
-      "git clone https://github.com/AstroUB/AstroUB && cd AstroUB && python3 -m astro"
+      "git clone https://github.com/LuciferUB/LuciferUB && cd LuciferUB && python3 -m lucifer"
 )
 # if something is wrong 
-print("You Have Got Error While Deployment Report and Get Help from @Astro_HelpChat")
+print("You Have Got Error While Deployment Report and Get Help from @Lucifer_support_group")
 # Script written for stop heroku suspension 
