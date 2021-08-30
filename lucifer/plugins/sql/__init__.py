ca925f82acb1cab1bcd55fffd745b787b0938886
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-08-30 18:45:15 +0530

    Update __init__.py

--- lucifer/plugins/sql/__init__.py
+++ lucifer/plugins/sql/__init__.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
  
 
-from astro.config import Config
+from lucifer.config import Config
 
 
 def start() -> scoped_session:
