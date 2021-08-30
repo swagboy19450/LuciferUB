72627cb9ae9af4981fb400fba46d5d2b783c9da8
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-08-30 18:17:59 +0530

    Add files via upload

---
+++ lucifer/plugins/assistant/pmbot/incoming.py
@@ -0,0 +1,23 @@
+# Astro-UB #
+from astro.plugins.assistant.sql.users_sql import add_user_to_db
+from astro.plugins.assistant.sql.blacklist_sql import check_is_black_list
+from telethon import events
+from astro.plugins import OWNER_ID
+
+# if incoming
+
+@tgbot.on(events.NewMessage(func=lambda e: e.is_private))
+async def one_new_mssg(event):
+    incoming = event.raw_text
+    who = event.sender_id
+    if check_is_black_list(who):
+        return
+    if incoming.startswith("/"):
+        pass
+    elif who == OWNER_ID:
+        return
+    else:
+        await event.get_sender()
+        event.chat_id
+        to = await event.forward_to(OWNER_ID)
+        add_user_to_db(to.id, who, event.id)
