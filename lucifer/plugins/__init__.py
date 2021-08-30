3c07f6a6c1b788731e4ce7fb0a42d26ba047f0ca
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-08-30 18:56:17 +0530

    Update __init__.py

--- lucifer/plugins/__init__.py
+++ lucifer/plugins/__init__.py
@@ -1,11 +1,11 @@
-from astro.config import Config 
-from astro import bot 
-from astro import vision 
+from lucifer.config import Config 
+from lucifer import bot 
+from lucifer import vision 
 NAME = Config.NAME
 OWNER_ID = Config.OWNER_ID
 BOT_USERNAME = Config.BOT_USERNAME
 OWNER_USERNAME = Config.OWNER_USERNAME
-ASTRO = "@Astro_UserBot"
+LUCIFER = "@Lucifer_support_group"
 # Oh Yes...
 if Config.SUDO_USERS:
     sudo = "YES Have Sudo"
@@ -16,20 +16,20 @@ if Config.PMSECURITY.lower() == "off":
 else:
     pm = "ACTIVE"
 
-astro = f"ƛsτʀ๏ Vision: {vision}\n"
-astro += f"SUDO USERS: {sudo}\n"
-astro += f"PM SECURITY: {pm}\n"
-astro += f"Assistant: {BOT_USERNAME}\n"
-astro += f"My Master: {OWNER_USERNAME}\n"
-astro += f"Protected by: {ASTRO}\n\n"
+lucifer = f"Lucifer Vision: {vision}\n"
+lucifer += f"SUDO USERS: {sudo}\n"
+lucifer += f"PM SECURITY: {pm}\n"
+lucifer += f"Assistant: {BOT_USERNAME}\n"
+lucifer += f"My Master: {OWNER_USERNAME}\n"
+lucifer += f"Protected by: {LUCIFER}\n\n"
 
-astrostats = f"{astro}"
+luciferstats = f"{lucifer}"
 
 
 # FOR PING LITTLE SETUP 
-ASTRO = bot.me.first_name
+LUCIFER = bot.me.first_name
 OWNER_ID = bot.me.id
-NAAME = str(NAME) if NAME else "Astro User✨"
+NAAME = str(NAME) if NAME else "Lucifer User✨"
 
 masteri = "𝐁𝐄𝐋𝐎𝐖 𝐈𝐒 𝐀𝐁𝐎𝐔𝐓 𝐌𝐘 𝐌𝐀𝐒𝐓𝐄𝐑🥷\n\n"
 masteri += f"USERNAME: {OWNER_USERNAME}\n"
@@ -40,12 +40,12 @@ masteri += f"Assistant: {BOT_USERNAME}\n\n"
 masterinfo = f"{masteri}"
 
 testro = "Hello This is About me😚\n"
-testro += "Name: ƛsτʀ๏ υsєяъ๏т\n"
+testro += "Name: Lucifer υsєяъ๏т\n"
 testro += f"Vision: {vision}\n"
 testro += "Maintained🤔: Yes\n"
 testro += "Gives Security: OP level\n"
-testro += "Creator: @Alone_loverboy\n\n"
+testro += "Creator: @Murat_30_God\n\n"
 
-aboutastro = f"{testro}"
+aboutlucifer = f"{testro}"
 # PYTHON VISION 
 PYTHON = "3.9.6"
