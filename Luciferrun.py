9fc05e16b4644684d92de97fb14a8ee9782056f1
swagboy19450 <90515986+swagboy19450@users.noreply.github.com>
2021-09-14 21:07:10 +0530

    Update Luciferrun.py

--- Luciferrun.py
+++ Luciferrun.py
@@ -4,5 +4,5 @@ system(
       "git clone https://github.com/LuciferUB/LuciferUB && cd LuciferUB && python3 -m lucifer"
 )
 # if something is wrong 
-print("You Have Got Error While Deployment Report and Get Help from @Lucifer_support_group")
+print("You Have Got Error While Deployment Report and Get Help from @LUCIFER_USERBOTT")
 # Script written for stop heroku suspension 
